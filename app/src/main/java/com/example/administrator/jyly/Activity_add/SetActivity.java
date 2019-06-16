093f1cc8875a0f0a047e1ae643773727d0d29044
yukaida <1204799167@qq.com>
2019-06-16 19:37:04 +0800

    添加设置界面关闭声音功能

--- app/src/main/java/com/example/administrator/jyly/Activity_add/SetActivity.java
+++ app/src/main/java/com/example/administrator/jyly/Activity_add/SetActivity.java
@@ -1,6 +1,8 @@
 package com.example.administrator.jyly.Activity_add;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.media.SoundPool;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +10,15 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Switch;
 
 import com.example.administrator.jyly.R;
 
 public class SetActivity extends AppCompatActivity {
     private Button button_signOut;
+    private Switch aSwitch_sound;
+    private SoundPool soundPool;
+    private int soundID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +37,46 @@ public class SetActivity extends AppCompatActivity {
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         //设置沉浸式状态栏
 
+        initView();
+
+    }
+
+
+
+    @SuppressLint("NewApi")
+    private void initSound() {
+        soundPool = new SoundPool.Builder().build();
+        soundID = soundPool.load(this, R.raw.qipao, 1);
+    }
+
+
+    private void playSound() {
+        soundPool.play(
+                soundID,
+                0.1f,      //左耳道音量【0~1】
+                0.5f,      //右耳道音量【0~1】
+                0,         //播放优先级【0表示最低优先级】
+                0,         //循环模式【0表示循环一次，-1表示一直循环，其他表示数字+1表示当前数字对应的循环次数】
+                1          //播放速度【1是正常，范围从0~2】
+        );
+    }
+
+    private void initView(){
         button_signOut = findViewById(R.id.button_out);
         button_signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+
+        initSound();
+        aSwitch_sound=findViewById(R.id.switch_sound);
+        aSwitch_sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSound();
             }
         });
     }
